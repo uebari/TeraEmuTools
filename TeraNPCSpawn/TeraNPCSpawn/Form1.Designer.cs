e9fb961b68b4260f4f3a208523954c7274c57cde
Uebari <uebari@live.com>
2013-05-17 01:24:24 -0400

    NPC Spawn Editor / XML Convertor

---
+++ TeraNPCSpawn/TeraNPCSpawn/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace TeraNPCSpawn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNPCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.saveNPCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.saveNPCToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(829, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exotToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exotToolStripMenuItem
+            // 
+            this.exotToolStripMenuItem.Name = "exotToolStripMenuItem";
+            this.exotToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.exotToolStripMenuItem.Text = "&Exit";
+            this.exotToolStripMenuItem.Click += new System.EventHandler(this.exotToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNPCToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // addNPCToolStripMenuItem
+            // 
+            this.addNPCToolStripMenuItem.Name = "addNPCToolStripMenuItem";
+            this.addNPCToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.addNPCToolStripMenuItem.Text = "Add NPC";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(169, 452);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // saveNPCToolStripMenuItem
+            // 
+            this.saveNPCToolStripMenuItem.Name = "saveNPCToolStripMenuItem";
+            this.saveNPCToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.saveNPCToolStripMenuItem.Text = "Save NPC";
+            this.saveNPCToolStripMenuItem.Click += new System.EventHandler(this.saveNPCToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 505);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exotToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNPCToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem saveNPCToolStripMenuItem;
+    }
+}
+
---
+++ TeraNPCSpawn/TeraNPCSpawn/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Data;
+using Data.Structures.Npc;
+using Data.Structures.Creature;
+using Data.Structures.Template;
+using Data.Interfaces;
+using Utils;
+using ProtoBuf;
+using System.IO;
+using Data.DAO;
+using System.Xml;
+
+
+namespace TeraNPCSpawn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public OpenFileDialog ofd = new OpenFileDialog();
+        public SaveFileDialog sfd = new SaveFileDialog();
+        public List<NpcTemplate> NpcTemplates = new List<NpcTemplate>();
+        public static Dictionary<int, List<SpawnTemplate>> Spawns = new Dictionary<int, List<SpawnTemplate>>();
+        public static List<SpawnTemplate> SpawnsList = new List<SpawnTemplate>();
+
+        public void LoadNPCList()
+        {
+
+        }
+
+        public void AddNPC()
+        {
+
+        }
+
+        public void SaveNPCList()
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.Cancel)
+                return;
+
+            Spawns = new Dictionary<int, List<SpawnTemplate>>();
+
+            using (FileStream stream = File.OpenRead(ofd.FileName))
+            {
+                while (stream.Position < stream.Length)
+                {
+                    SpawnTemplate spawnTemplate = Serializer.DeserializeWithLengthPrefix<SpawnTemplate>(stream, PrefixStyle.Fixed32);
+
+                    if (!Spawns.ContainsKey(spawnTemplate.MapId))
+                        Spawns.Add(spawnTemplate.MapId, new List<SpawnTemplate>());
+
+                    Spawns[spawnTemplate.MapId].Add(spawnTemplate);
+
+                    listBox1.Items.Add(spawnTemplate.NpcId);
+                    SpawnsList.Add(spawnTemplate);
+                }
+            }
+
+        }
+
+        private void LoadSpawnInfo()
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult ds = sfd.ShowDialog();
+            if (ds == DialogResult.Cancel)
+                return;
+
+            using (FileStream fs = File.OpenRead(ofd.FileName))
+            {
+                SpawnTemplate spawnTemplate = Serializer.DeserializeWithLengthPrefix<SpawnTemplate>(fs, PrefixStyle.Fixed32);
+                using (FileStream stream = File.OpenWrite(sfd.FileName))
+                {
+                    foreach (var sp in SpawnsList)
+                    {
+                        //Serializer.SerializeWithLengthPrefix(stream, sp, PrefixStyle.Fixed32);
+                    }
+                }
+                using (StreamWriter writer = new StreamWriter(sfd.FileName))
+                {
+                    foreach (var sp in SpawnsList)
+                    {
+                        string XMLFormat = "<NPC ID=\"" + sp.NpcId + "\""
+                            + " Type=\"" + sp.Type + "\" MapId=\"" + sp.MapId + "\""
+                            + " X=\"" + sp.X + "\" Y=\"" + sp.Y + "\" Z=\"" + sp.Z + "\""
+                            + " Heading=\"" + sp.Heading + "\"></NPC>";
+                        writer.WriteLine(XMLFormat);
+                    }
+                }
+                SpawnsList.Clear();
+            }
+        }
+
+        private void exotToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            //object NpcID = listBox1.SelectedItem;
+            //tbNpcId.Text = NpcID.ToString();
+            //int NpcIDInt = int.Parse(NpcID.ToString());
+            //MessageBox.Show(SpawnsList[NpcIDInt].NpcId.ToString());
+        }
+
+        private void saveNPCToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.Cancel)
+                return;
+            SpawnsList.Clear();
+            XmlReaderSettings xrset = new XmlReaderSettings();
+            xrset.ConformanceLevel = ConformanceLevel.Fragment;
+            using (XmlReader reader = XmlReader.Create(ofd.FileName, xrset))
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "NPC")
+                    {
+                        SpawnsList.Add(new SpawnTemplate
+                        {
+                            NpcId = int.Parse(reader.GetAttribute(0)),
+                            Type = short.Parse(reader.GetAttribute(1)),
+                            MapId = int.Parse(reader.GetAttribute(2)),
+                            X = float.Parse(reader.GetAttribute(3)),
+                            Y = float.Parse(reader.GetAttribute(4)),
+                            Z = float.Parse(reader.GetAttribute(5)),
+                            Heading = short.Parse(reader.GetAttribute(6))
+                        });
+                    }
+                }
+                reader.Close();
+
+                DialogResult ds = sfd.ShowDialog();
+                if (ds == DialogResult.Cancel)
+                    return;
+
+                using (FileStream stream = File.OpenWrite(sfd.FileName))
+                {
+                    foreach ( var sp in SpawnsList )
+                    {
+                        Serializer.SerializeWithLengthPrefix(stream, sp, PrefixStyle.Fixed32);
+                    }
+               }
+            }
+        }
+    }
+}
